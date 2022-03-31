aa447706104e37663c8256bd426f1ee1668d1c27
Shreyank Buddhadev <shreyankbuddhadev99@gmail.com>
2022-03-31 16:54:16 +0530

    Added Sign In/Up pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,28 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Signup from "./pages/Signup";
+// import Login from "./pages/Login";
+// import Dashboard from "./pages/Dashboard";
+// import Signup from "./pages/Signup";
 
 // //importing api
 // import { getTest } from "./api/test";
+
+import SignIn from "./components/signin/SignIn";
+import SignUp from "./components/signup/SignUp";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" exact element={<Signup />} />
+          {/* <Route path="/" exact element={<Signup />} />
           <Route path="/login" exact element={<Login />} />
-          <Route path="/dashboard" exact element={<Dashboard />} />
+          <Route path="/dashboard" exact element={<Dashboard />} /> */}
+          <Route path="/SignIn" exact element={<SignIn />} />
+          <Route path="/SignUp" exact element={<SignUp />} />
+          <Route path="/" exact element={<SignUp />} />
+
         </Routes>
       </BrowserRouter>
     </div>
