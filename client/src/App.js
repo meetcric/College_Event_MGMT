ebf9703b8a568a374fd1e0229302f6b5b786895d
Shreyank <shreyankbuddhadev99@gmail.com>
2022-04-28 00:04:52 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import SignIn from "./components/signin/SignIn";
